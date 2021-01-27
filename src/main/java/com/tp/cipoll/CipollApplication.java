d31d7f16f6388197a9b83aae3ccab3bd6cf996fb
Blessie Suresh <sureshblessie@gmail.com>
2021-01-27 10:08:52 +0100

    springboot initial application

---
+++ src/main/java/com/tp/cipoll/CipollApplication.java
@@ -0,0 +1,13 @@
+package com.tp.cipoll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CipollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CipollApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tp/cipoll/CipollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tp.cipoll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CipollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
